e199cc0d93e9a9ed2ad78150a265b64877a8b521
Morten Poul Jensen <mpj@foreno.dk>
2017-10-10 21:49:31 +0200

    Add PresenterCommand to enable the use of make:presenter.

---
+++ src/Commands/PresenterCommand.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace Olymbytes\Presenters\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class FilterCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:presenter';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Presenter class.';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Presenter';
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return config('presenters.namespace');
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/stubs/presenter.stub';
+    }
+}
